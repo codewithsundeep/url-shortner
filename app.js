8b56b5812a19181bfb712c960ed1fbc762c13d4d
Sundeep Sharma <77357224+codewithsundeep@users.noreply.github.com>
2023-04-30 20:08:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const crypto = require("crypto");
+app.set("view engine", "hbs");
+app.use(express.json());
+app.use(express.static("static"));
+function shorts() {
+  let short = require("./shorts.json");
+  return short.data;
+}
+function shortAndSave(url) {
+  return new Promise((resolve, reject) => {
+    // console.log("url is", url);
+    let all = shorts();
+    let id = crypto.randomBytes(2).toString("hex");
+    let obj = {};
+    obj[id] = url;
+    let ft = all.filter((x) => x[id]);
+    // console.log("all is", all);
+    if (ft.length < 1) {
+      all.push(obj);
+      let newjson = {
+        data: all,
+      };
+      //   console.log("new json is", newjson);
+      fs.writeFile("shorts.json", JSON.stringify(newjson), (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve({ long: url, short: id });
+        }
+      });
+    } else {
+      reject("Something went wrong!");
+    }
+  });
+}
+
+app.get("/", (req, res) => {
+  res.render("main");
+});
+app.post("/", async (req, res) => {
+  //   console.log(req.body.url);
+  try {
+    let dt = await shortAndSave(req.body.url);
+    res.send(dt);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+app.get("/recent", (req, res) => {
+  let sort = require("./shorts.json");
+  let data = sort.data;
+  if (data.length < 11) {
+    res.json(JSON.stringify({ data: data }));
+  } else {
+    let spc = data.slice(data.length - 10);
+    res.json(JSON.stringify({ data: spc }));
+  }
+});
+app.get("/*", (req, res) => {
+  let short = require("./shorts.json");
+  let data = short.data;
+  let path = req.path.replace("/", "");
+  let ft = data.filter((x) => x[path]);
+  if (ft.length < 1) {
+    res.redirect("/");
+  } else {
+    res.redirect(ft[0][path]);
+  }
+});
+app.listen(process.env.PORT || 3000);
---
+++ static/short.js
@@ -0,0 +1,37 @@
+const form = document.querySelector("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let url = document.querySelector("#longurl");
+  let obj = { url: url.value };
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(obj),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      url.value = document.URL + data.short;
+      recent();
+    })
+    .catch((err) => alert(err));
+});
+
+function recent() {
+  fetch("/recent")
+    .then((res) => res.json())
+    .then((data) => {
+      let parse = JSON.parse(data);
+      let reducer = parse.data.reduce((acc, cur) => {
+        let id = Object.entries(cur)[0][0];
+        let shortUrl = document.URL + id;
+        return (
+          acc +
+          ` <li class="mb-1"><i
+            class="fas fa-long-arrow-alt-right me-2 text-info"
+          ></i><a href="${shortUrl}">${shortUrl}</a></li>`
+        );
+      }, "");
+      document.querySelector("#recents").innerHTML = reducer;
+    });
+}
+recent();
